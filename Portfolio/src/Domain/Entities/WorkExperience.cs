4b9a8b4fe578020e6dc7a75b22bbfcdbf45f06fe
Ryan Cortino <ryancortino@outlook.com>
2022-09-01 06:56:33 -0500

    Adds WorkExperience domain object.

---
+++ Portfolio/src/Domain/Entities/WorkExperience.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace Portfolio.Domain.Entities;
+
+public class WorkExperience
+{
+    public string Name { get; init; }
+    public string Location;
+    public string Dates;
+    public string JobTitle;
+
+    public string[] Responsibilities;
+    public string[] Skills;
+    public string[] Strengths;
+}
